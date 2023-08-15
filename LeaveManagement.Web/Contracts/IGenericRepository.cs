4745ac2ee40d003a41a2fac4c9ead88632dcb332
m-tonon <math.tonon@gmail.com>
2023-08-15 07:51:58 -0300

    feat: setup async database repository

---
+++ LeaveManagement.Web/Contracts/IGenericRepository.cs
@@ -0,0 +1,13 @@
+using LeaveManagement.Web.Data;
+
+namespace LeaveManagement.Web.Contracts;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> AddAsync(T entity);
+    Task DeleteAsync(int id);
+    Task<bool> Exists(int id);
+    Task<List<T>> GetAllAsync();
+    Task<T> GetAsync(int id);
+    Task UpdateAsync(T entity);
+}
